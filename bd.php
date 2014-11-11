29f06206e12740dea616b90facac18d3b3d68845
angel lacret <alacret@gmail.com>
2014-11-10 20:43:38 -0430

    come commit2

--- bd.php
+++ bd.php
@@ -9,10 +9,8 @@ if (get_current_user() === "vagrant") {
     $password = "root";
     $db = "collecttweets";
 }
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
-
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
